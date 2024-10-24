d74470490dab26998101348c7ccf10d95226c0d1
Jitender2105 <jitender2105@gmail.com>
2024-10-24 13:45:24 +0530

    testing

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Backend is working');
+  res.send('Backend is working..');
 });
 
 const PORT = process.env.PORT || 5000;
